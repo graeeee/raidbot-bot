7ac289edd7235e58b700fd5391416fc90f9ab0ab
graeeee <46288884+graeeee@users.noreply.github.com>
2019-02-02 00:44:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ client.on("message", async message => {
         }
     }
 });
+//10manqueue1
 client.on('voiceStateUpdate', (oldMember, newMember) => {
     let newUserChannel = newMember.voiceChannel;
     let oldUserChannel = oldMember.voiceChannel;
@@ -99,7 +100,31 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
             // user moved from one voice channel to another (old channel ID is different from the new one)
             let hahaembed = new Discord.RichEmbed()
                 .setColor("#FF0000")
-                .addField(`10man Queue Logs`, `${newMember} left queue`)
+                .addField(`10man Queue Logs`, `${newMember} left **#10man queue #1**`)
+                .setTimestamp();
+                client.channels.get('530786248198062091').send(hahaembed)
+    }
+});
+//10manqueue2
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    let newUserChannel = newMember.voiceChannel;
+    let oldUserChannel = oldMember.voiceChannel;
+
+    if(!oldUserChannel && newUserChannel) {
+        if (oldMember.voiceChannelID !== '486205232066461711' && newMember.voiceChannelID !== '486205232066461711') return;
+        // user joined a channel without being in one previously (old channel is undefined, new channel is defined)
+
+        let botembed = new Discord.RichEmbed()
+            .setColor("#32CD32")
+            .addField(`10man Queue Logs`, `${newMember} joined voice channel **${newUserChannel}**`)
+            .setTimestamp();
+            client.channels.get('530786248198062091').send(botembed)
+                } else if (oldMember.voiceChannelID !== undefined) {
+            if (oldMember.voiceChannelID !== '486205232066461711' && newMember.voiceChannelID !== '486205232066461711') return;
+            // user moved from one voice channel to another (old channel ID is different from the new one)
+            let hahaembed = new Discord.RichEmbed()
+                .setColor("#FF0000")
+                .addField(`10man Queue Logs`, `${newMember} left **#10man queue #2**`)
                 .setTimestamp();
                 client.channels.get('530786248198062091').send(hahaembed)
     }

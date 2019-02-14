e91929bc76af65f382d83b213390c7729d2747bb
Jacob Hixon <jacobhixon47@gmail.com>
2019-02-13 22:15:38 -0600

    add missing line and .json file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const botconfig =require("./botconfig.json");
 const Discord = require("discord.js");
 const client = new Discord.Client({diableEveryone: true})
+// this is the line we were missing.
+client.tempMutedUsers = require('./temp-muted-users.json');
 
 client.on("ready", async () => {
     console.log(`${client.user.username} is online!`);

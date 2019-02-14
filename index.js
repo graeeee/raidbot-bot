60fab00381df12412b4abb1e7b221dfef02677ec
graeeee <46288884+graeeee@users.noreply.github.com>
2019-02-13 21:41:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,67 +18,9 @@ client.on("message", async message => {
        
         let botembed = new Discord.RichEmbed()
         .setColor("#2ecc71")
-        .addField("On Duty", "*If you would like to go OnDuty, use the command !onduty to get notifications on reports.*")
-        .addField("Off Duty", "*If you would like to go OffDuty, use the command !offduty to not get notifications on reports.*");
+        .addField("On Duty", "*If you would like to go OnDuty, use the command **rb-onduty** to get notifications on reports.*")
+        .addField("Off Duty", "*If you would like to go OffDuty, use the command **rb-offduty** to not get notifications on reports.*");
         message.channel.send(botembed);
-    }
-});
-//onduty role
-client.on("message", async message => {
-    let prefix = botconfig.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-    let onduty = (`${prefix}onduty`)
-   
-    if (message.channel.id === '529421034781147156') {
-        if((message.content!=onduty)) {
-            await message.delete(5000)
-            message.author.send('Please use the command !onduty or !offduty.');
-        }
-        else{
-            if(cmd === onduty){
-                let botembed = new Discord.RichEmbed()
-                .setColor("#008000")
-                .addField(`OnDuty Rank Given`,`${message.author} is now OnDuty.`);  
-                message.channel.send(botembed);
-                let OnDuty = message.member.guild.roles.find("name", "OnDuty");
-                message.member.addRole(OnDuty)
-                let OffDuty = message.member.guild.roles.find("name", "OffDuty");
-                message.member.removeRole(OffDuty)
-                await message.delete()
-              
-            }
-        }
-    }
-});
-//offduty role
-client.on("message", async message => {
-    let prefix = botconfig.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-    let offduty = (`${prefix}offduty`)
-   
-    if (message.channel.id === '529421034781147156') {
-        if((message.content!=offduty)) {
-            await message.delete(5000)
-            message.author.send('Please use the command !offduty.');
-        }
-        else{
-            if(cmd === offduty){
-                let botembed = new Discord.RichEmbed()
-                .setColor("#008000")
-                .addField(`OffDuty Rank Given`,`${message.author} is now OffDuty.`);  
-                message.channel.send(botembed);
-                let OffDuty = message.member.guild.roles.find("name", "OffDuty");
-                message.member.addRole(OffDuty)
-                let OnDuty = message.member.guild.roles.find("name", "OnDuty");
-                message.member.removeRole(OnDuty)
-                await message.delete()
-              
-            }
-        }
     }
 });
 //10manqueue logs 1

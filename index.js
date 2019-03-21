702c0158ff4cc946f228a25c0fec411a795319bf
graeeee <46288884+graeeee@users.noreply.github.com>
2019-03-20 23:09:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -242,7 +242,7 @@ client.on("message", async message => {
     if (cmd === `${prefix}servers`) {
 	        let botembed = new Discord.RichEmbed()
                 .setColor("#808080")
-                .addField(`[Raidboss Servers]`,`Click any links below to automatically join that raidboss server.\n<:raidboss:392574240643219456> • [Raidboss] 5v5 Comp #1 [!WS !KNIFE | Monthly Ladder | 128 Tick] [[Join]](https://steam://connect/192.223.26.107:27015) \n<:raidboss:392574240643219456> • [Raidboss] 5v5 Comp #2 [!WS !KNIFE | Monthly Ladder | 128 Tick] [[Join]](https://steam://connect/192.223.27.140:27015)`);  
+                .addField(`[Raidboss Servers]`,`Click any links below to automatically join that raidboss server.\n\n<:raidboss:392574240643219456> • [Raidboss] 5v5 Comp #1 [!WS !KNIFE | Monthly Ladder | 128 Tick] [[Join]](https://steam://connect/192.223.26.107:27015) \n<:raidboss:392574240643219456> • [Raidboss] 5v5 Comp #2 [!WS !KNIFE | Monthly Ladder | 128 Tick] [[Join]](https://steam://connect/192.223.27.140:27015)`);  
                 message.channel.send(botembed);
     }
 });
